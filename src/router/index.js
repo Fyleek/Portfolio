5d66e6b13642e9f5639bbe4e8d18366f773c99f8
Oscar <odecloquement@gmail.com>
2021-04-15 10:48:14 +0200

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Error404 from "../views/Error404";
-import RIP_EMA from "../views/RIP_EMA";
 
 Vue.use(VueRouter)
 
@@ -13,11 +12,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/ema',
-    name: 'EMA',
-    component: RIP_EMA
-  },
-  {
     path: '/404',
     name: 'Error 404',
     component: Error404
@@ -26,14 +20,6 @@ const routes = [
     path: "*",
     redirect: "/404"
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
