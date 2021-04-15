8cc041656f7a9d2ff6ee0b4eab2f2d22031747e9
Oscar <odecloquement@gmail.com>
2021-04-15 14:02:05 +0200

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Error404 from "../views/Error404";
-import Ema from "../views/RIP_EMA";
 
 Vue.use(VueRouter)
 
@@ -11,11 +10,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/ema',
-    name: 'EMA',
-    component: Ema
   },
   {
     path: '/404',
